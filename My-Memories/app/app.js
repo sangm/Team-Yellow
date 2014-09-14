8130e4b487a9647096546a0a0b281b61c20f4674
sangm <sang.mercado@gmail.com>
2014-09-14 18:51:04 -0500

    Prototype

--- My-Memories/app/app.js
+++ My-Memories/app/app.js
@@ -1,5 +1,28 @@
-angular.module('app', []);
+angular.module('app', ['ngRoute', 'ngTouch']);
 
-angular.module('app').controller('MainController', function($scope) {
-    $scope.test = "123123123"
-});
+angular.module('app')
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'partials/main.html',
+                controller: 'MainController'
+            })
+            .when('/add', {
+                templateUrl: 'partials/add.html',
+                controller: 'AddController'
+            })
+            .otherwise({redirectTo: '/'})
+    });
+
+angular.module('app')
+    .controller('MainController', function($scope) {
+        $scope.test = "123123123"
+    })
+    .controller('AddController', function($scope) {
+        
+    });
+
+angular.module('app')
+    .service('PhotoService', function() {
+        
+    });
