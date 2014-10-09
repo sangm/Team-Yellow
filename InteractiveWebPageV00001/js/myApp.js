dbc68cf8dabe8c22c7b1d06dac7842432dad5423
jm2362 <jm2362@txstate.edu>
2014-10-09 12:49:23 -0500

    title factory added

--- InteractiveWebPageV00001/js/myApp.js
+++ InteractiveWebPageV00001/js/myApp.js
@@ -1,5 +1,25 @@
 var myApp = angular.module("myApp", []);
 
+myApp.factory('myFactory', function() {
+	var pageInfo = {title: 'Page Title'};
+
+	var factory = {};
+
+	factory.getTitle = function() {
+		return pageInfo.title;
+	};
+
+	factory.postTitle = function(pgTitle) {
+		pageInfo.title = pgTitle;
+	};
+
+	return factory;
+});
+
+myApp.controller("PageCtrl", function($scope, myFactory) {
+	$scope.pageTitle = myFactory.getTitle();
+});
+
 myApp.controller("TabCtrl", function($scope) {
 	$scope.tabValue = -1;
 	$scope.setTab = function(setTab) {
@@ -16,11 +36,9 @@ myApp.controller("TabCtrl", function($scope) {
 	};
 });
 
-/*
-myApp.directive('offCanvasNav', function() {
-	return {
-		restrict: 'E',
-		templateUrl: "Tempates/off-canvas-nav.html"
+myApp.controller("TitleCtrl", function($scope, myFactory) {
+	$scope.title = '';
+	$scope.changeTitle = function() {
+		myFactory.postTitle($scope.title);
 	};
 });
-*/
