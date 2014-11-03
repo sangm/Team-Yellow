72027f1a987d2c78382b946228fd97f41bf9315c
Sang Mercado <sang.mercado@mgial.com>
2014-11-03 05:11:25 +0000

    Adding routes and domain functions

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,8 +1,6 @@
 from flask import Flask
-from flask.ext import restful
 
 app = Flask(__name__)
-rest = restful.Api(app)
 
-import api.resources
+import api.routes
 
--- api/api/routes.py
+++ api/api/routes.py
@@ -1,8 +1,13 @@
 from api import app
+from utils import redis_get_domains, redis_insert_domain
+from flask import jsonify
 
 @app.route('/domains')
 def get_domains():
-    return ""
+    return jsonify(domains=redis_get_domains())
 @app.route('/domain/<domain>', methods=['PUT'])
 def insert_domain(domain):
-    return "Got domain name: %s" % domain
+    if redis_insert_domain(domain):
+        return "Inserted domain: %s" % domain
+    else:
+        return "Could not insert domain: %s" % domain
