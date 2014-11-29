042a218141de2de2bae80bded72b8f4707a96679
sangm <sang.mercado@gmail.com>
2014-11-29 17:06:13 -0500

    Moved out all the dependent variables into __init__.py file

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 cors = CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
-app.config['TEMPLATE_DIRECTORY'] = '/tmp/templates'
+app.config['TEMPLATE_DIRECTORY'] = '/var/www/templates'
 app.config['VALID_KEYS'] = ['businessName', 'businessEmail', 'template', 'phoneNumber']
 app.config['VALID_TEMPLATES'] = ['so-boxy', 'banded', 'contact']
+app.config['NGINX_DIRECTORY'] = '/etc/nginx/sites-enabled'
+app.config['NGINX_TEMPLATE'] = '/var/www/ares.sangm.net/api/api/templates/nginx'
 
 import api.routes
 import api.utils
--- api/api/utils/redis_domain.py
+++ api/api/utils/redis_domain.py
@@ -3,8 +3,8 @@ import redis
 redis_server = redis.StrictRedis('localhost')
 publish = redis_server.pubsub()
 redis_domains = "Domains"
-template_directory = "/home/sangm/templates"
-nginx_directory = "/home/sangm/subdomains"
+TEMPLATE_LOOKUP_DIRECTORY = "/var/www/templates"
+NGINX_OUTPUT_DIRECTORY = "/etc/nginx/sites-enabled"
 
 def redis_get_domains():
     return redis_server.lrange(redis_domains, 0, -1)
@@ -15,8 +15,8 @@ def redis_insert_domain(domain):
         redis_server.lpush(redis_domains, domain)
         redis_server.publish('DomainChannel', domain)
         return redis_server.hmset(domain, {'ServerName': domain,
-                                           'Template' : template_directory,
-                                           'Nginx': nginx_directory})
+                                           'Template' : TEMPLATE_LOOKUP_DIRECTORY,
+                                           'Nginx': NGINX_OUTPUT_DIRECTORY})
 def redis_delete_domain(domain):
     result = redis_server.hdel(domain, 'ServerName')
     result |= redis_server.hdel(domain, 'Directory')
