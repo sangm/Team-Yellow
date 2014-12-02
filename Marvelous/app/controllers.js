7ed8c469cfbb0f9d2b7539684dd94483a0aabae2
Jesus <Jesus@Dalek.local>
2014-12-01 23:36:32 -0600

    Adding alert messages

--- Marvelous/app/controllers.js
+++ Marvelous/app/controllers.js
@@ -8,4 +8,18 @@ angular.module('app')
             else if (type == 'domainName')    TemplateService.setDomainName(info);
             $scope.businessInfo = TemplateService.getBusinessInfo();
         };
+	$scope.registerDomain = function(businessInfo) {
+            console.log(businessInfo);
+            // Check if values are null
+            // and hostname
+            //Isn't working at the moment, not sure why
+            if (businessInfo.businessName === "")
+                alert("Business Name field is empty!")
+	    else if (businessInfo.phoneNumber === "")
+		alert("Phone number field is empty!")
+	    else if (businessInfo.domainName === "")
+		alert("Domain name field is empty!")
+	    else if (businessInfo.businessEmail === "")
+		alert("Email field is emtpy!")
+	    console.log("Hmmm"); //What is this for?
     })
